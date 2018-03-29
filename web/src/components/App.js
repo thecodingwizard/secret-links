0b39e673f173e9e92c9e7d537246594716debf90
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-28 21:32:54 -0700

    add material ui

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Button from 'material-ui/Button';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Hello World!
+				<Button variant="raised" color="primary">
+					Hello World!
+				</Button>
 			</div>
 		);
 	}
