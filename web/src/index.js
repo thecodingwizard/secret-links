cd4225871b00f76f39d2930ba0e9d919a5e35801
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-28 22:22:19 -0700

    add redux

--- web/src/index.js
+++ web/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
 import './styles/styles.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = configureStore();
+
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
 registerServiceWorker();
