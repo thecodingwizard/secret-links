390866be947dad9782928a98654092abde311e30
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-30 22:30:52 -0700

    add service

--- src/services/links.service.js
+++ src/services/links.service.js
@@ -1,4 +1,15 @@
-export function fetchLink(accessUrl) {
-	console.log(accessUrl);
-	return "hi";
+import "whatwg-fetch";
+
+const SERVER = "https://secretlinksapi.pandadevgroup.com";
+
+export function fetchLink(accessUrl, password = "testing") {
+	return fetch(`${SERVER}/links/${accessUrl}`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ password })
+	}).then(response => response.json()).then(response => {
+		console.log(response);
+	});
 }
