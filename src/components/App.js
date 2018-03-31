7b3fce90544b07d29e0f5cf50002bafe06ee57da
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-30 23:17:28 -0700

    add toolbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import SearchIcon from "material-ui-icons/Search";
 
 import HomePage from "./home/HomePage";
 import ViewLinkPage from "./view-link/ViewLinkPage";
@@ -8,6 +14,21 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<header>
+					<AppBar position="static">
+						<Toolbar>
+							<IconButton className="toolbar__menu" color="inherit" aria-label="Menu">
+								<MenuIcon />
+							</IconButton>
+							<Typography variant="title" color="inherit" className="toolbar__title">
+								Secret Links
+							</Typography>
+							<IconButton color="inherit" aria-label="Search">
+								<SearchIcon />
+							</IconButton>
+						</Toolbar>
+					</AppBar>
+				</header>
 				<main>
 					<Route path="/" exact component={HomePage} />
 					<Route path="/links/:linkUrl" component={ViewLinkPage} />
