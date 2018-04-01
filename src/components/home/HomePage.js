b4a14e8f00e365c57423641d897c56eee8d62719
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-31 17:25:01 -0700

    add home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,9 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { withStyles } from 'material-ui/styles';
 import Button from "material-ui/Button";
+import Typography from "material-ui/Typography";
 import AddIcon from "material-ui-icons/Add";
 
 const styles = theme => ({
+	container: {
+		maxWidth: "30rem",
+		margin: "0 auto",
+		padding: "1.5rem 1rem",
+		textAlign: "center",
+	},
+	title: {
+		textAlign: "center",
+		fontSize: "2rem",
+		marginBottom: "1rem"
+	},
 	button: {
 		margin: theme.spacing.unit,
 		position: "fixed",
@@ -14,6 +26,9 @@ const styles = theme => ({
 			bottom: "1rem",
 			right: "1rem",
 		}
+	},
+	text: {
+		margin: "1rem 0"
 	}
 });
 
@@ -23,9 +38,17 @@ class HomePage extends React.Component {
 
 		return (
 			<div>
-				Home Page
-				<br/>
-				<Link to="/links/pandadevgroup">Example Link</Link>
+				<div className={classes.container}>
+					<Typography variant="headline" className={classes.title}>
+						Secret Links
+					</Typography>
+					<Typography className={classes.text}>
+						A simple Progressive Web App that lets you encrypt links.
+					</Typography>
+					<Typography className={classes.text}>
+						Try it out! Start by clicking the "+" button on the bottom right corner.
+					</Typography>
+				</div>
 
 				<Link to="/links/new">
 					<Button variant="fab" color="secondary" aria-label="add" className={classes.button}>
