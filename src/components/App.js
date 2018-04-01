181e3a51ea7bfbefb5d1bc3aeb809ae4eb9c1015
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-31 17:40:27 -0700

    add sidenav

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,52 @@
 import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
+import { withStyles } from "material-ui/styles";
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
+import SwipeableDrawer from 'material-ui/SwipeableDrawer';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import MenuIcon from 'material-ui-icons/Menu';
 import SearchIcon from "material-ui-icons/Search";
+import HomeIcon from "material-ui-icons/Home";
+import AddIcon from "material-ui-icons/Add";
 
 import HomePage from "./home/HomePage";
 import ViewLinkPage from "./view-link/ViewLinkPage";
 import NewLinkPage from "./new-link/NewLinkPage";
 import SearchPage from "./search/SearchPage";
 
+const styles = {
+	list: {
+		width: 300,
+	}
+};
+
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			drawerOpen: false
+		};
+	}
+
+	toggleDrawer = (open) => () => {
+		this.setState({
+			drawerOpen: open
+		});
+	}
+
 	render() {
+		const { classes } = this.props;
 		return (
 			<div>
 				<header>
 					<AppBar position="static">
 						<Toolbar>
-							<IconButton className="toolbar__menu" color="inherit" aria-label="Menu">
-								<MenuIcon />
+							<IconButton className="toolbar__menu" color="inherit" aria-label="Menu" onClick={this.toggleDrawer(true)}>
+								<MenuIcon/>
 							</IconButton>
 							<Typography variant="title" color="inherit" className="toolbar__title">
 								<Link to="/">Secret Links</Link>
@@ -34,6 +60,39 @@ class App extends React.Component {
 						</Toolbar>
 					</AppBar>
 				</header>
+				<SwipeableDrawer
+					open={this.state.drawerOpen}
+					onClose={this.toggleDrawer(false)}
+    			    onOpen={this.toggleDrawer(true)}>
+					<div
+						tabIndex={0}
+						role="button"
+						onClick={this.toggleDrawer(false)}
+						onKeyDown={this.toggleDrawer(false)}>
+						<div className={classes.list}>
+							<List>
+								<ListItem button>
+									<ListItemIcon>
+										<HomeIcon />
+									</ListItemIcon>
+									<ListItemText primary="Home" />
+								</ListItem>
+								<ListItem button>
+									<ListItemIcon>
+										<SearchIcon />
+									</ListItemIcon>
+									<ListItemText primary="Search" />
+								</ListItem>
+								<ListItem button>
+									<ListItemIcon>
+										<AddIcon />
+									</ListItemIcon>
+									<ListItemText primary="Create New Link" />
+								</ListItem>
+							</List>
+						</div>
+					</div>
+				</SwipeableDrawer>
 				<main>
 					<Switch>
 						<Route path="/" exact component={HomePage} />
@@ -47,4 +106,4 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+export default withStyles(styles)(App);
